0bf1702821fceee32ebeed1fa495f447d17f5427
Ilan Copelyn <ilan@netreturn.co.za>
2018-07-03 21:13:27 +0200

    prettier

--- src/uploader/uploader.js
+++ src/uploader/uploader.js
@@ -3,118 +3,122 @@
 let dropArea = document.getElementById("drop-area");
 
 // Prevent default drag behaviors
-['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
-  dropArea.addEventListener(eventName, preventDefaults, false)
-  document.body.addEventListener(eventName, preventDefaults, false)
+["dragenter", "dragover", "dragleave", "drop"].forEach(eventName => {
+  dropArea.addEventListener(eventName, preventDefaults, false);
+  document.body.addEventListener(eventName, preventDefaults, false);
 });
 
 // Highlight drop area when item is dragged over it
-['dragenter', 'dragover'].forEach(eventName => {
-  dropArea.addEventListener(eventName, highlight, false)
+["dragenter", "dragover"].forEach(eventName => {
+  dropArea.addEventListener(eventName, highlight, false);
 });
 
-['dragleave', 'drop'].forEach(eventName => {
-  dropArea.addEventListener(eventName, unhighlight, false)
+["dragleave", "drop"].forEach(eventName => {
+  dropArea.addEventListener(eventName, unhighlight, false);
 });
 
 // Handle dropped files
-dropArea.addEventListener('drop', handleDrop, false)
+dropArea.addEventListener("drop", handleDrop, false);
 
 function preventDefaults(e) {
-  e.preventDefault()
-  e.stopPropagation()
+  e.preventDefault();
+  e.stopPropagation();
 }
 
 function highlight(e) {
-  dropArea.classList.add('highlight')
+  dropArea.classList.add("highlight");
 }
 
 function unhighlight(e) {
-  dropArea.classList.remove('highlight')
+  dropArea.classList.remove("highlight");
 }
 
 function handleDrop(e) {
-  var dt = e.dataTransfer
-  var files = dt.files
+  var dt = e.dataTransfer;
+  var files = dt.files;
 
-  handleFiles(files)
+  handleFiles(files);
 }
 
-let uploadProgress = []
-let progressBar = document.getElementById('progress-bar')
+let uploadProgress = [];
+let progressBar = document.getElementById("progress-bar");
 
 function initializeProgress(numFiles) {
-  progressBar.value = 0
-  uploadProgress = []
+  progressBar.value = 0;
+  uploadProgress = [];
 
   for (let i = numFiles; i > 0; i--) {
-    uploadProgress.push(0)
+    uploadProgress.push(0);
   }
 }
 
 function updateProgress(fileNumber, percent) {
-  uploadProgress[fileNumber] = percent
-  let total = uploadProgress.reduce((tot, curr) => tot + curr, 0) / uploadProgress.length
-  console.debug('update', fileNumber, percent, total)
-  progressBar.value = total
+  uploadProgress[fileNumber] = percent;
+  let total =
+    uploadProgress.reduce((tot, curr) => tot + curr, 0) / uploadProgress.length;
+  console.debug("update", fileNumber, percent, total);
+  progressBar.value = total;
 }
 
 function handleFiles(files) {
-  files = [...files]
-  initializeProgress(files.length)
-  files.forEach(uploadFile)
+  files = [...files];
+  initializeProgress(files.length);
+  files.forEach(uploadFile);
   // files.forEach(previewFile)
 }
 
 function previewFile(file) {
-  let reader = new FileReader()
-  reader.readAsDataURL(file)
-  reader.onloadend = function () {
-    let img = document.createElement('img')
-    img.src = reader.result
-    document.getElementById('gallery').appendChild(img)
-  }
+  let reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onloadend = function() {
+    let img = document.createElement("img");
+    img.src = reader.result;
+    document.getElementById("gallery").appendChild(img);
+  };
 }
 
 function uploadFile(file, i) {
-  let reader = new FileReader()
-  reader.onloadend = function (event) {
-    let base64Txt = event.target.result.replace(/^data:application\/pdf;base64,/, '');
+  let reader = new FileReader();
+  reader.onloadend = function(event) {
+    let base64Txt = event.target.result.replace(
+      /^data:application\/pdf;base64,/,
+      ""
+    );
     // cuntPdfRequest(file.name, base64Txt, undefined)
-    spikePdfRequest(file.name, base64Txt, undefined)
-  }
-  reader.readAsDataURL(file)
+    spikePdfRequest(file.name, base64Txt, undefined);
+  };
+  reader.readAsDataURL(file);
 }
 
 //////
 
-let prodApiHost = 'https://api-v5.spikedata.co.za';
-let debugApiHost = 'http://localhost:3000';
+let prodApiHost = "https://api-v5.spikedata.co.za";
+let debugApiHost = "http://localhost:3000";
 let settings = {
   apiHost: debugApiHost, //prodApiHost,
-  apiKey: '88246a40-88dc-11e7-bbe2-4f7182116a75', // public test key
-  userKey: 'ccf55bd0-898f-11e7-9851-a5eee2319d1d',
+  apiKey: "88246a40-88dc-11e7-bbe2-4f7182116a75", // public test key
+  userKey: "ccf55bd0-898f-11e7-9851-a5eee2319d1d"
 };
 
 const FetchResult = {
-  NoResponse: -2,  // didn't get response
+  NoResponse: -2, // didn't get response
   ServerError: -1, // got response from server (internal error)
   ApiSuccess: 0, // got Api response = success
   ApiSuccessNeedReq2: 1, // got Api response = success, but need a second request = e.g. OTP
-  ApiFail: 2, // got Api response = fail
+  ApiFail: 2 // got Api response = fail
 };
 
 async function spikeRequest(apiKey, userKey, url, inputs) {
   try {
     const response = await fetch(url, {
-      method: 'POST',
+      method: "POST",
       timeout: 120000,
       body: JSON.stringify(inputs),
       headers: {
-        'x-api-key': apiKey,
-        'x-user-key': userKey,
-        'accept': 'application/json',
-        'content-type': 'application/json'
+        "x-api-key": apiKey,
+        "x-user-key": userKey,
+        accept: "application/json",
+        "content-type": "application/json"
       }
     });
 
@@ -130,9 +134,18 @@ async function spikeRequest(apiKey, userKey, url, inputs) {
     const json = await response.json();
     if (json.result === "success") {
       var extracted = json.data[0];
-      return { result: FetchResult.ApiSuccess, requestId: json.requestId, data: extracted };
+      return {
+        result: FetchResult.ApiSuccess,
+        requestId: json.requestId,
+        data: extracted
+      };
     } else {
-      return { result: FetchResult.ApiFail, requestId: json.requestId, error: json.errorCode, message: json.message || json.messages };
+      return {
+        result: FetchResult.ApiFail,
+        requestId: json.requestId,
+        error: json.errorCode,
+        message: json.message || json.messages
+      };
     }
   } catch (ex) {
     // Didn't get a response: fetch threw an exception - e.g. network timeout
@@ -151,18 +164,29 @@ async function spikePdfRequest(filename, base64Txt, password) {
       pdfPass: password
     }
   };
-  let request = await spikeRequest(settings.apiKey, settings.userKey, url, inputs);
+  let request = await spikeRequest(
+    settings.apiKey,
+    settings.userKey,
+    url,
+    inputs
+  );
   if (request.result === FetchResult.ApiSuccess) {
     let trans = request.data.transactions;
     let validation = request.data.validation;
     if (validation && validation.length) {
-      console.warn('WARN', request.requestId, 'validation errors', filename, validation);
+      console.warn(
+        "WARN",
+        request.requestId,
+        "validation errors",
+        filename,
+        validation
+      );
     }
     if (!trans || trans.length === 0) {
-      console.warn('WARN', request.requestId, 'no transactions', filename);
+      console.warn("WARN", request.requestId, "no transactions", filename);
       return; //continue;
     }
-    console.warn('SUCCESS', request.requestId, filename);
+    console.warn("SUCCESS", request.requestId, filename);
     transactions = transactions.concat(trans);
   } else {
     console.error("FAILED", request.requestId, filename); // let us know if you have a valid .pdf which fails
@@ -170,7 +194,7 @@ async function spikePdfRequest(filename, base64Txt, password) {
   }
 
   if (!transactions || transactions.length === 0) {
-    console.warn('WARN', 'no transactions in any of the files');
+    console.warn("WARN", "no transactions in any of the files");
     return;
   }
 }
